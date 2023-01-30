17385b6eeedefe711c17e9e1e99df6e0ed1a99e8
Dharansh Neema <dharanshneema@gmail.com>
2023-01-31 01:05:40 +0530

    Final change for 30/01/23 added a form that can submit name

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const questions = require("./assets/script");
+const bodyParser = require("body-parser");
 // const { PORT } = process.env;
 const PORT = 8080;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(extend));
 app.get("/", (req, res) => {
   res.status(200).render("index.ejs");
 });
 app.get("/quiz", (req, res) => {
   res.status(201).render("quiz", { questions });
+});
+app.post("/user", (req, res) => {
+  const name = req.body;
 });
 app.listen(PORT, () => console.log(`On PORT ${PORT}`));
